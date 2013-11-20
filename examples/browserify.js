a592743adb2e47f4d33dc2df3547f1c8b4fcdd52
Anton Amurskiy <antonamurskiy@gmail.com>
2013-11-20 17:02:50 -0500

    browserified

---
+++ examples/browserify.js
@@ -0,0 +1,20 @@
+var visutils = require('../lib/visutils');
+console.log(visutils);
+var scales = require('../lib/scales');
+var bar = require('../lib/vis/bar');
+var cumulative = require('../lib/vis/cumulative');
+var lineDotted = require('../lib/vis/line-dotted');
+var line = require('../lib/vis/line');
+var chart = require('../lib/chart');
+
+var data = require('./browserify.json')
+
+mychart = document.createElement('figure')
+mychart.id = 'mychart'
+mychart.style.height = '500px';
+mychart.style.width = '500px';
+
+
+document.body.appendChild(mychart);
+
+stupidchart = new chart('bar', data, '#mychart');
